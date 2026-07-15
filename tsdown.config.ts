4e010d8e4a91d347ac36480f2bdc0a4df1cd9f60
Masonlet <masonletoile@hotmail.com>
2026-07-14 20:16:07 -0400

    fix: glob exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: [
-    'src/assets.ts',
-    'src/canvas.ts',
-    'src/input/keyboard.ts',
-    'src/input/pointer.ts',
-    'src/update.ts',
-    'src/validate.ts',
-  ],
+  entry: ['src/**/*.ts', '!src/**/*.test.ts'],
   format: 'esm',
   dts: true,
   sourcemap: true,
